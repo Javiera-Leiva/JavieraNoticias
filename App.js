be8723bcba6c9d3c012cd5a64bed99419bff8291
Javiera-Leiva <javieraleiva026@gmail.com>
2023-06-17 11:17:00 -0600

    Componente Stack con navegaciones

--- App.js
+++ App.js
@@ -1,12 +1,29 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+
+import NewsList from './src/NewsList';
+import NewsDetail from './src/NewsDetail';
+
+const Stack = createNativeStackNavigator();
 
 const App = () => {
   return (
-    <View>
-      <Text>App</Text>
-    </View>
-  )
-}
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen
+          name="NewsList"
+          component={NewsList}
+          options={{ title: 'Últimas noticias' }}
+        />
+        <Stack.Screen
+          name="NewsDetail"
+          component={NewsDetail}
+          options={{ title: 'Detalle de noticias' }}
+        />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+};
 
-export default App
+export default App;
